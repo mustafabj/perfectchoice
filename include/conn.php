7291678971c3c97a522f528e143d7c98e42e9a7f
mustafabj <mustafabojok@hotmail.com>
2025-03-05 01:19:40 +0300

    change the database connection

--- include/conn.php
+++ include/conn.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 
-$conn = mysqli_connect("sql898.main-hosting.eu", "u695315962_perfectchoice", '1x$FedKAN99A', "u695315962_perfectchoice");
+$conn = mysqli_connect("srv898.hstgr.io", "u695315962_perfectchoice", "1tv;9&t:#kU", "u695315962_perfectchoice");
 
 if (!$conn) {
     die("cannot connect to Server");
