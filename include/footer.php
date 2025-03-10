331ed87e96eb9945bcbac32ef94971908e589507
mustafabj <mustafabojok@hotmail.com>
2025-03-10 12:29:31 +0300

    change the footer links

--- include/footer.php
+++ include/footer.php
@@ -1,23 +1,23 @@
 <footer>
     <div class="container">
         <div class="contact">
-            <a href="tel:"><i class="fa-solid fa-mobile-screen-button"></i></a>
-            <a href=""
+            <a href="tel:00962799000929"><i class="fa-solid fa-mobile-screen-button"></i>00962799000929</a>
+            <a href="https://maps.app.goo.gl/LuhYKmMtFRgqWen29"
                 target="_blank"><i class="fa-solid fa-location-dot"></i>Our
                 Location
             </a>
-            <a href="mailto:"><i
-                    class="fa-solid fa-envelope"></i></a>
+            <a href="mailto:perfecttchoicejo@gmail.com"><i
+                    class="fa-solid fa-envelope"></i>perfecttchoicejo@gmail.com</a>
         </div>
         <div class="logo">
             <img src="image/Perfect Choice-01.png" alt="" srcset="" />
         </div>
         <div class="social">
-            <a href="https://www.facebook.com/" target="_blank"><i
+            <a href="https://www.facebook.com/share/15fJqvmMZa/" target="_blank"><i
                     class="fa-brands fa-facebook"></i></a>
-            <a href="https://www.instagram.com/" target="_blank"><i
+            <a href="https://www.instagram.com/perfectchoice.ez?igsh=d2hnNGVxeHJnMXF4" target="_blank"><i
                     class="fa-brands fa-instagram"></i></a>
-            <a href="https://api.whatsapp.com/send?phone=" target="_blank"><i
+            <a href="https://api.whatsapp.com/send?phone=00962799000929" target="_blank"><i
                     class="fa-brands fa-whatsapp"></i></a>
         </div>
     </div>
--- include/footerAr.php
+++ include/footerAr.php
@@ -1,22 +1,22 @@
 <footer>
     <div class="container">
         <div class="contact">
-            <a href="tel:00962795566535"><i class="fa-solid fa-mobile-screen-button"></i>0795566535</a>
-            <a href="https://www.google.com/maps/place/XVP5%2B94+Torch+for+Equipments+%26+Electronics+Trading,+Amman,+Jordan/@31.9859375,35.8578125,15z/data=!4m6!3m5!1s0x151ca1162cfed3d9:0x547498b99007be0b!8m2!3d31.9859375!4d35.8578125!16s%2Fg%2F11k3tlvt4c?hl=en-GB"
+            <a href="tel:00962799000929"><i class="fa-solid fa-mobile-screen-button"></i>00962799000929</a>
+            <a href="https://maps.app.goo.gl/LuhYKmMtFRgqWen29"
                 target="_blank"><i class="fa-solid fa-location-dot"></i>موقعنا
             </a>
-            <a href="mailto:torch4electronics@gmail.com"><i
-                    class="fa-solid fa-envelope"></i>torch4electronics@gmail.com</a>
+            <a href="mailto:perfecttchoicejo@gmail.com"><i
+                    class="fa-solid fa-envelope"></i>perfecttchoicejo@gmail.com</a>
         </div>
         <div class="logo">
-            <img src="../image/Torch-all1.png" alt="" srcset="" />
+            <img src="../image/Perfect Choice-01.png" alt="" srcset="" />
         </div>
         <div class="social">
-            <a href="https://www.facebook.com/profile.php?id=100088488919496&mibextid=ZbWKwL" target="_blank"><i
+            <a href="https://www.facebook.com/share/15fJqvmMZa/" target="_blank"><i
                     class="fa-brands fa-facebook"></i></a>
-            <a href="https://www.instagram.com/itorch4electronics/?igshid=MDM4ZDc5MmU%3D" target="_blank"><i
+            <a href="https://www.instagram.com/perfectchoice.ez?igsh=d2hnNGVxeHJnMXF4" target="_blank"><i
                     class="fa-brands fa-instagram"></i></a>
-            <a href="https://api.whatsapp.com/send?phone=962795566535" target="_blank"><i
+            <a href="https://api.whatsapp.com/send?phone=00962799000929" target="_blank"><i
                     class="fa-brands fa-whatsapp"></i></a>
         </div>
     </div>
